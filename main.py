2c66e8f78f19e3a39d319bcb0cebe1dc101183c1
Abduaws <www.abduaws@gmail.com>
2023-04-25 00:16:15 +0200

    NFA to DFA Functionality Added + Error Handling

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class GraphInputWindow(QtWidgets.QMainWindow):
         self.addEdgeBtn.clicked.connect(self.addEdge_click)
         self.addNodeBtn.clicked.connect(self.addNode_click)
         self.resetBtn.clicked.connect(self.reset_click)
-        self.convertBtn.clicked.connect(lambda: print("Done"))
+        self.convertBtn.clicked.connect(self.convert_click)
         self.backBtn.clicked.connect(self.back_click)
         self.graphvizGraph = graphviz.Digraph()
         self.inputEdge.returnPressed.connect(self.addEdge_click)
@@ -56,21 +56,34 @@ class GraphInputWindow(QtWidgets.QMainWindow):
         self.graphEdges = defaultdict(lambda: list(), self.graphEdges)
         self.startNode = ""
         self.finalNodes = []
+        self.nodes = []
         lay = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
         lay.setContentsMargins(0, 0, 0, 0)
         lay.addWidget(self.frame)
         self.scrollArea.setWidgetResizable(True)
         self.frame.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
         self.scale = 1.0
+        self.transitionTable = defaultdict(lambda: defaultdict(lambda: list()))
+        self.dfaTable = defaultdict(lambda: defaultdict(lambda: ""))
+        self.transitions = set()
+        self.convertCheck = True
 
     def back_click(self):
         MainWidget.setCurrentIndex(0)
 
     def reset_click(self):
+        self.graphEdges = {}
+        self.graphEdges = defaultdict(lambda: list(), self.graphEdges)
+        self.startNode = ""
+        self.finalNodes = []
+        self.nodes = []
+        self.scale = 1.0
+        self.transitionTable = defaultdict(lambda: defaultdict(lambda: list()))
+        self.dfaTable = defaultdict(lambda: defaultdict(lambda: ""))
+        self.transitions = set()
+        self.draw_graph()
         self.graphvizGraph.clear()
         self.graphEdges.clear()
-        self.draw_graph()
-        self.scale = 1.0
         cleanFiles()
 
     def draw_graph(self):
@@ -79,26 +92,39 @@ class GraphInputWindow(QtWidgets.QMainWindow):
         self.graphEdges.clear()
         if len(dotGraph.get_edges()) > 0:
             for edge in dotGraph.get_edges():
-                if edge.get_source() == "": continue
+                if edge.get_source() == '""': continue
                 self.graphEdges[(edge.get_source(), edge.get_destination())].append(edge.get('label').replace('"', "").replace(' ', ""))
         dotGraph.write_png(resource_path("Resources/graph.png"))
         self.frame.setPixmap(QtGui.QPixmap(resource_path("Resources/graph.png")))
 
     def addEdge_click(self):
+        if self.convertCheck:
+            self.convertCheck = False
+            self.reset_click()
         try:
             inputs = self.inputEdge.text()
             decompose = inputs.split(",")
             if len(decompose) > 3: raise Exception
+            if str(decompose[2]) in self.graphEdges[(str(decompose[0]), str(decompose[1]))]:
+                self.error_popup("Edge Already Added!!")
+                return
             self.graphvizGraph.edge(str(decompose[0]), str(decompose[1]), label=str(" "+decompose[2]))
+            self.transitions.add(decompose[2])
             self.draw_graph()
         except Exception as e:
             print(e)
             self.error_popup("Please Enter Edge In Correct Format", "Format Example: 1,2,A or 2,5,B")
 
     def addNode_click(self):
+        if self.convertCheck:
+            self.convertCheck = False
+            self.reset_click()
         try:
             node = self.inputNode.text()
             if node.find(" ") != -1: raise Exception
+            if node in self.nodes:
+                self.error_popup("Node Already Added!!")
+                return
             if self.nodeType.currentText() == "Starting":
                 if self.startNode != "": raise Exception
                 self.graphvizGraph.node(str(node), color="red", fontcolor="red")
@@ -106,15 +132,16 @@ class GraphInputWindow(QtWidgets.QMainWindow):
                 self.graphvizGraph.edge("", str(node), label="")
                 self.startNode = node
             elif self.nodeType.currentText() == "Final":
-                self.graphvizGraph.node(node, color="green", fontcolor="green")
+                self.graphvizGraph.node(node, color="darkgreen", fontcolor="darkgreen")
                 self.finalNodes.append(node)
             else: self.graphvizGraph.node(node)
+            self.nodes.append(node)
             self.draw_graph()
         except Exception as e:
             print(e)
-            self.error_popup("Please Enter Node In Correct Format", "Format Example: 1 or 2\n\nNotes:\n\t"
-                                                                    "1)No spaces are allowed\n\t"
-                                                                    "2)No more than 1 starting node")
+            self.error_popup("Please Enter Node In Correct Format", "Format Example: 1 or 2\n\nNotes:\n"
+                                                                    "1) No spaces are allowed\n"
+                                                                    "2) No more than 1 starting node")
 
     def done_click(self):
         self.inputMenu.setCurrentIndex(0)
@@ -140,6 +167,64 @@ class GraphInputWindow(QtWidgets.QMainWindow):
         pixmapSize = pixmap.size()
         newpixmap = pixmap.scaled(self.scale * pixmapSize, QtCore.Qt.KeepAspectRatio)
         self.frame.setPixmap(newpixmap)
+
+    def convert_click(self):
+        self.convertCheck = True
+        if len(self.finalNodes) == 0 or not(self.startNode):
+            self.error_popup("Please Make Sure there is One Starting state and At least One Final State!")
+            return
+        try:
+            self.constructTransitionTable()
+            visitedNodes = []
+            nonVisitedNodes = [[self.startNode]]
+            while nonVisitedNodes:
+                currNode = nonVisitedNodes.pop()
+                if currNode in visitedNodes: continue
+                visitedNodes.append(currNode)
+                miniTrans = defaultdict(lambda: list())
+                for node in currNode:
+                    for transition, nodes in self.transitionTable[node].items():
+                        miniTrans[transition].extend(nodes)
+                        miniTrans[transition] = [*set(miniTrans[transition])]
+                for transition, nodes in miniTrans.items():
+                    self.dfaTable[str(currNode)][str(transition)] = str(nodes)
+                    if self.finalNodes[0] in nodes: self.finalNodes.append(nodes)
+                    if (nodes in visitedNodes) or (nodes == currNode):continue
+                    nonVisitedNodes.append(nodes)
+            self.displayDFA()
+        except Exception as e:
+            print(e)
+            self.reset_click()
+            self.error_popup("Make Sure you entered the NFA Correctly!")
+
+    def constructTransitionTable(self):
+        for (node1, node2), transitions in self.graphEdges.items():
+            for transition in transitions:
+                self.transitionTable[str(node1)][str(transition)].append(str(node2))
+
+    def displayDFA(self):
+        self.graphEdges = {}
+        self.graphEdges = defaultdict(lambda: list(), self.graphEdges)
+        self.nodes = []
+        self.scale = 1.0
+        self.transitionTable = defaultdict(lambda: defaultdict(lambda: list()))
+        self.graphvizGraph.clear()
+        self.graphEdges.clear()
+        for node, transitionDict in self.dfaTable.items():
+            for transition in self.transitions:
+                if self.dfaTable[node][transition]:
+                    self.graphvizGraph.edge(str(node), str(self.dfaTable[node][transition]), label=str(" "+transition))
+                else:
+                    self.graphvizGraph.edge(str(node), "Failed", label=str(" " + transition))
+        self.graphvizGraph.node(str([self.startNode]), color="red", fontcolor="red")
+        self.graphvizGraph.node("", width="0.01", height="0.01")
+        self.graphvizGraph.edge("", str([self.startNode]), label="")
+        if self.startNode in self.finalNodes: self.finalNodes.append([self.startNode])
+        for finNode in self.finalNodes[1:]:
+            self.graphvizGraph.node(str(finNode), color="darkgreen", fontcolor="darkgreen")
+        self.startNode = ""
+        self.finalNodes = []
+        self.draw_graph()
 
 
 if __name__ == "__main__":
